28f67a25c140c73d1614781b2a11587156207659
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-06-28 02:44:47 +0900

    beckjoon 10998

---
+++ M4/M4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class M4
+{
+    private string? _input;
+    private string[] _inputStrings = new string[2];
+    static void Main(string[] args)
+    {
+        M4 Program = new M4();
+        Program.Run();
+    }
+
+    void Run()
+    {
+        _input = Console.ReadLine();
+        if (_input == null)
+            throw new ArgumentException("input null!");
+
+        _inputStrings = _input.Split(" ");
+        int sum = int.Parse(_inputStrings[0]) * int.Parse(_inputStrings[1]);
+
+        System.Console.Write(sum);
+    }
+}
