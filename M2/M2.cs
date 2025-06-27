8b7ddb0c7e5d35740dba6e04931110fee5dc9850
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-06-27 23:56:50 +0900

    1000 / 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오

---
+++ M2/M2.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int[] nums = new int[2];
+        string? input = Console.ReadLine();
+        if (input == null)
+        {
+            throw new ArgumentException("input is null!");
+        }
+        string[] inputs = input.Split(" ");
+
+        for (int i = 0; i < 2; i++)
+        {
+            nums[i] = int.Parse(inputs[i]);
+            sum += nums[i];
+        }
+        if (nums[0] <= 0 || nums[1] >= 10)
+        {
+            throw new ArgumentException("num range error!");
+        }
+
+        System.Console.Write(sum);
+    }
+}
