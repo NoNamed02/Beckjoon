af41232cbe7b40ffeb00873761234f81d30cf5fe
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-06-28 02:43:48 +0900

    .

---
+++ M3/M3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class M3
+{
+    static void Main(string[] args)
+    {
+        string? _input = Console.ReadLine();
+        if (_input == null)
+            throw new ArgumentException("input is null!");
+        string[] _inputSplit = _input.Split(" ");
+        System.Console.Write(int.Parse(_inputSplit[0]) - int.Parse(_inputSplit[1]));
+    }
+}
