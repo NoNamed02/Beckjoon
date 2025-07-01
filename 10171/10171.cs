ffbd8949e7ed53988eda9c3d4e6bc17041776d85
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-07-02 01:00:41 +0900

    10171 \, ' 등의 문자에 주의하며 고양이를 출력하는 문제

---
+++ 10171/10171.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class M10171
+{
+    static void Main()
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
