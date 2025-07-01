6b3833c9186ea928970eadb7043e560071f7acf0
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-07-02 00:41:29 +0900

    10926 입출력을 응용하는 문제??!

---
+++ 10926/10926.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+class M10926
+{
+    StringBuilder _stringbulider = new StringBuilder();
+    static void Main()
+    {
+        M10926 m10926 = new M10926();
+        m10926.run();
+    }
+
+    void run()
+    {
+        string? input = Console.ReadLine();
+        _stringbulider.Append(input + "??!");
+        Console.Out.Write(_stringbulider.ToString());
+    }
+}
