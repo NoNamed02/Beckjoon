bfe48874087638d1127381f56efa7bbdb634fece
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-07-04 16:10:15 +0900

    14681 사분면 고르기

---
+++ 14681/14681.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class M14681
+{
+    static void Main()
+    {
+        string? input = Console.ReadLine();
+        int posX = int.Parse(input);
+        input = Console.ReadLine();
+        int posY = int.Parse(input);
+
+        int Out = posX > 0 && posY > 0 ? 1 :
+                    posX > 0 && posY < 0 ? 4 :
+                    posX < 0 && posY > 0 ? 2 :
+                    3;
+        Console.Write(Out);
+    }
+}
