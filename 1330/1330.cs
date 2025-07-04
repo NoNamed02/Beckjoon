2f2af4e2300588ceddce42356cc87ee2d767ecf5
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-07-04 15:21:10 +0900

    1330	두 수 비교하기

---
+++ 1330/1330.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class M1330
+{
+    static void Main()
+    {
+        string? input = Console.ReadLine();
+        if (input == null)
+            throw new ArithmeticException("input null");
+
+        short[] value = input.Split(" ").Select(short.Parse).ToArray();
+        string check = value[0] > value[1] ? ">" : value[0] < value[1] ? "<" : "=="; 
+
+        Console.Write(check);
+    }
+}
