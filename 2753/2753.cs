a740fcd1dc764458cfd9bf908af8f89d1131cf18
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-07-04 15:57:16 +0900

    2753	윤년

---
+++ 2753/2753.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class M2753
+{
+    static void Main()
+    {
+        int? value = int.Parse(Console.ReadLine());
+        int Out = (value % 4 == 0 && value % 100 != 0) || value % 400 == 0 ? 1 : 0;
+        Console.Write(Out);
+    }
+}
