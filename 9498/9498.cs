c966e7341575a4c2663ab03243400aefc3cda5a5
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2025-07-04 15:21:19 +0900

    9498	시험 성적

---
+++ 9498/9498.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class M9498
+{
+    static void Main()
+    {
+        string? input = Console.ReadLine();
+        if (input == null)
+            throw new ArgumentException("input null");
+        int value = int.Parse(input);
+        char grade = value >= 90 ? 'A' : value >= 80 ? 'B' : value >= 70 ? 'C' : value >= 60 ? 'D' : 'F';
+
+        Console.Write(grade);
+    }
+}
